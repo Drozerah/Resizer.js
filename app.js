3e396feb6ce9c09f175751c6324daa8f7d56b6b2
Drozerah <drozerah@gmail.com>
2019-09-06 18:22:15 +0200

    feat: working with events and time
    - add object to constructor:
    	- _interval to bring interval timer settings
    - create methods:
    	- startInterval(value) add a setInterval() timer to increment
    	_input value
    	- stopInterval() to clear any running timer
    - events:
    	- onClick(elm) now toggle the timer to start or stop according
    	to external boolean
    	- inputEvent(elm) now stop the timer if allready running
    - fix: CSS button width
    - docs: add comments for better code readability

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ class Resizer {
 
     constructor(){
 
+        /**
+        * DOM elements bindings 
+        */
         this._wrapper = document.getElementById('resizer-wrapper')
-        this._input = document.querySelector('#resizer input[type="range"]')
         this._output = document.querySelector('#resizer output')
+        this._input = document.querySelector('#resizer input[type="range"]')
         this._button = document.querySelector('#resizer button')
+        /**
+        * Proxy instanciation 
+        */
         this._proxy = new Proxy(this._input, {
 
             set: (target, prop, value) => {
@@ -21,32 +27,87 @@ class Resizer {
 
                 this.output(value) // update _output
 
+                console.log(`value: ${value}`) // #DEBUG
+
                 return true
             }
         })
-
+        /**
+        * Timer interval 
+        */
+        this._interval = {
+            handler: undefined,
+            isRunning: false,
+            step: 1,
+            duration: 1000 // ms
+        }
     }
 
-    output(value){
-        this._output.innerHTML = `${value}%`
-    }
+    output(value){this._output.innerHTML = `${value}%`}
 
-    proxy(value){
-        this._proxy.valueAsNumber = value
-    }
+    proxy(value){this._proxy.valueAsNumber = value} // update _input value definition
 
     inputEvent(elm){
-        elm.addEventListener('input', e => this.proxy(e.target.valueAsNumber), false)
+
+        elm.addEventListener('input', e => {
+            
+            // stop interval if running
+            if (this._interval.isRunning) {
+
+                this.stopInterval() // clear Interval
+
+            }
+
+            this.proxy(e.target.valueAsNumber) // update _input value through proxy
+
+        }, false)
     }
 
     onClick(elm){
-        elm.addEventListener('click', () => this.proxy(100), false)
+
+        elm.addEventListener('click', () => {
+
+
+            if (!this._interval.isRunning) {
+        
+                this.startInterval(this._input.valueAsNumber) // set Interval
+
+            } else {
+
+                this.stopInterval() // clear Interval
+
+            }
+
+        }, false)
+    }
+
+    startInterval(value){
+
+        this._interval.isRunning = true // update isRunning boolean
+        this._button.innerHTML = `Stop` // update button text
+        this._interval.handler = setInterval(() => { // set Interval
+
+            value = value + this._interval.step
+
+            this.proxy(value) // update _input value through proxy
+
+        }, this._interval.duration)
+   
+    }
+
+    stopInterval(){
+
+        this._interval.isRunning = false // update isRunning boolean
+        this._button.innerHTML = `Start` // update button text
+        clearInterval(this._interval.handler) // clear Interval
+
     }
 
     init(){
+
         this.output(this._input.value) // update _output by default
-        this.inputEvent(this._input)
-        this.onClick(this._button)
+        this.inputEvent(this._input) // initialize 'input' event listener
+        this.onClick(this._button)  //  initialize 'click' event listener
         return true
     }
 }
