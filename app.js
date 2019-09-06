b4f5dafab8f0f181cfa848f091d03deb30769c5c
Drozerah <drozerah@gmail.com>
2019-09-06 14:06:54 +0200

    feat: add bindings, methods and proxy
    - bind elements to constructor:
    	- _resize-wrapper
    	- _output
    	- _button
    	- _proxy
    - instanciate a new Proxy Object that points to the _input element
    - create methods:
    	- output(value) to update the _output element with a given _input
    	value
    	- proxy(value) to pass in the Proxy a given _input value
    	- inputEvent(elm) to add one event listener that calls the
    	proxy() method
    	- onClick(elm) to add a 'click' event listener that calls the
    	proxy() method
    - CSS fix the #resize padding

--- app.js
+++ app.js
@@ -9,12 +9,44 @@ class Resizer {
 
     constructor(){
 
-        this._input = document.querySelector('input[type="range"]')
+        this._wrapper = document.getElementById('resizer-wrapper')
+        this._input = document.querySelector('#resizer input[type="range"]')
+        this._output = document.querySelector('#resizer output')
+        this._button = document.querySelector('#resizer button')
+        this._proxy = new Proxy(this._input, {
+
+            set: (target, prop, value) => {
+
+                target[prop] = value // update proxy target
+
+                this.output(value) // update _output
+
+                return true
+            }
+        })
 
     }
 
+    output(value){
+        this._output.innerHTML = `${value}%`
+    }
+
+    proxy(value){
+        this._proxy.valueAsNumber = value
+    }
+
+    inputEvent(elm){
+        elm.addEventListener('input', e => this.proxy(e.target.valueAsNumber), false)
+    }
+
+    onClick(elm){
+        elm.addEventListener('click', () => this.proxy(100), false)
+    }
+
     init(){
-        console.log(this._input)
+        this.output(this._input.value) // update _output by default
+        this.inputEvent(this._input)
+        this.onClick(this._button)
         return true
     }
 }
